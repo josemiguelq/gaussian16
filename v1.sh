9a85d580479bb867dd7ce8c0061d67b90f060bc6
Jose Miguel <jose.miguelq95@gmail.com>
2022-08-04 21:43:59 -0300

    added log file

--- v1.sh
+++ v1.sh
@@ -4,15 +4,15 @@ OUTPUT_PATH="triazol-Cu-opt.log"
 INPUT_PATH="coordinates_input.xyz"
 
 start_time=$SECONDS
-sleep 5
 g16 < $1 > OUTPUT_PATH
 elapsed=$(( SECONDS - start_time ))
 
 # Parse to last geometry
 python parse.py
 
-# Notify
-curl -X POST https://api.telegram.org/$TELEGRAM_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="Finalizado+Opt+Tempo gasto: $elapsed seg"
+# Notify and send input file
+curl -X POST https://api.telegram.org/$TELEGRAM_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="Finalizado+Opt+Tempo gasto: $elapsed seg. Arquivo gerado: ->"
+curl -F document=@"$INPUT_PATH" https://api.telegram.org/$TELEGRAM_TOKEN/sendDocument?chat_id=$CHAT_ID
 
 # Using the first gaussian output
 g16 < $INPUT_PATH > "$2.log"
